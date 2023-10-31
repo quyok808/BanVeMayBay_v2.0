7a84c9106df15baecd8d84314163940781af4f69
CongQuy252 <nva2572003@gmail.com>
2023-10-31 16:21:36 +0700

    Done Login_Change-pass

--- QLBVMB_v2.0/Program.cs
+++ QLBVMB_v2.0/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using QLBVMB_v2._0.Login_Register;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -16,7 +17,7 @@ namespace QLBVMB_v2._0
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new Main_Form());
         }
     }
 }
